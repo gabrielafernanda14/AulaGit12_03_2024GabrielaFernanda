cedecf80aa790ec96b303589027eed03eb6b3314
gabriela fernanda <gabrielafernanda.tds2.senai@gmail.com>
2024-03-12 16:51:08 -0300

    adicionei mensagem linha 10

--- EXtemperatura.js
+++ EXtemperatura.js
@@ -7,7 +7,7 @@ let mensagem1 = "graus celcius na sua casa igual a ";
 console.log(mensagem1 + celcius);
 
 let c = 50;
-
+//let c vezes 1.8
 let fahrenheit = (c * 1.8) + 32;
 //coloquei uma mensagem
 let mensagem2 = "fahrenheit na sua casa igual a ";

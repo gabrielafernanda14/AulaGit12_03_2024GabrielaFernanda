87386bc4121a2ae0c40b751fc424a82ff1f9f6cf
gabriela fernanda <gabrielafernanda.tds2.senai@gmail.com>
2024-03-12 16:57:58 -0300

    comentario na linha 8

--- EXtemperatura.js
+++ EXtemperatura.js
@@ -5,7 +5,7 @@ let celcius = (ft - 32) / 1.8;
 let mensagem1 = "graus celcius na sua casa igual a ";
 
 console.log(mensagem1 + celcius);
-
+//defini let C
 let c = 50;
 //let c vezes 1.8
 let fahrenheit = (c * 1.8) + 32;

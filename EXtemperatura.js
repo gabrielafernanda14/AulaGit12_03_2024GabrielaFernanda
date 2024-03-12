77884727f91bd1f3ef19e1c3004d5f20ab28817b
gabriela fernanda <gabrielafernanda.tds2.senai@gmail.com>
2024-03-12 15:34:14 -0300

    mudei o let ft

---
+++ EXtemperatura.js
@@ -0,0 +1,15 @@
+let ft = 222;
+
+let celcius = (ft - 32) / 1.8;
+
+let mensagem1 = "graus celcius na sua casa igual a ";
+
+console.log(mensagem1 + celcius);
+
+let c = 50;
+
+let fahrenheit = (c * 1.8) + 32;
+
+let mensagem2 = "fahrenheit na sua casa igual a ";
+
+console.log(mensagem2 + fahrenheit);
